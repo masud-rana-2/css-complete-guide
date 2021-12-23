19c87933dee1118c700745bdd39422ef41d34655
masud-rana-2 <cont.masudrana@gmail.com>
2021-12-23 19:16:45 +0600

    add many feature

---
+++ shared.js
@@ -0,0 +1,44 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+// console.dir(mobileNav);
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = "open"; // overwrite
+    backdrop.classList.add("open");
+    modal.classList.add("open");
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  backdrop.classList.remove("open");
+  if (modal) {
+    modal.classList.remove("open");
+  }
+}
+
+toggleButton.addEventListener("click", function () {
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
